f993cfd3e872b6314cca0700cd0a83edc98121a1
Jon Bone <bonejon@gmail.com>
2019-01-18 11:26:28 +0000

    replaced common service with mock

--- src/app/store/common/common.spec.ts
+++ src/app/store/common/common.spec.ts
@@ -1,14 +1,14 @@
 import { cold } from 'jasmine-marbles';
+import { of } from 'rxjs';
+import { Store } from '@ngrx/store';
+
+import { CommonService } from '../../common/services/common.service';
+import { PersonTitle } from '../../common/models/persontitle';
+
 import * as commonActions from './common.actions';
 import { CommonEffects } from './common.effects';
-import { CommonService } from '../../common/services/common.service';
-import { Store, StoreModule } from '@ngrx/store';
-import { configureTestSuite } from 'ng-bullet';
-import { TestBed } from '@angular/core/testing';
-import { CommonState, initialCommonState } from './common.state';
-import { PersonTitle } from '../../common/models/persontitle';
 import { commonReducer } from './common.reducer';
-import { Observable, of } from 'rxjs';
+import { CommonState, initialCommonState } from './common.state';
 
 describe('NGRX: common state', () => {
   describe('reducers', () => {
@@ -49,31 +49,32 @@ describe('NGRX: common state', () => {
   });
 
   describe('effects', () => {
-    it('GetPersonTitleAction should be called when state is empty', () => {
-      const emptyStoreSpy = jasmine.createSpyObj('emptyStoreSpy', [
-        'dispatch', 'subscribe'
-      ]) as Store<CommonState>;
-
-      emptyStoreSpy.select = () => of([]);
-
+    fit('GetPersonTitleAction should be called when state is empty', () => {
       const expectedTitles: Array<PersonTitle> = [
         new PersonTitle('Mr', 1),
         new PersonTitle('Mrs', 2),
         new PersonTitle('Miss', 3)
       ];
 
+      const emptyStoreSpy = jasmine.createSpyObj('emptyStoreSpy', [
+        'dispatch', 'subscribe'
+      ]) as Store<CommonState>;
+
+      emptyStoreSpy.select = () => of([]);
+
+      const commonServiceSpy = {
+        getTitles: jasmine.createSpy('getTitles').and.returnValue(of(expectedTitles))
+      } as CommonService;
+
       const source = cold('a', { a: new commonActions.GetPersonTitlesAction() });
 
-      const commonService: CommonService = new CommonService();
-      spyOn(commonService, 'getTitles').and.callThrough();
-
-      const effects = new CommonEffects(commonService, emptyStoreSpy, source);
+      const effects = new CommonEffects(commonServiceSpy, emptyStoreSpy, source);
 
       const expected = cold('b', { b: new commonActions.GetPersonTitlesSuccessAction(expectedTitles) });
 
       expect(effects.loadPersonTitles$).toBeDefined();
       expect(effects.loadPersonTitles$).toBeObservable(expected);
-      expect(commonService.getTitles).toHaveBeenCalledTimes(1);
+      expect(commonServiceSpy.getTitles).toHaveBeenCalledTimes(1);
     });
 
     it('GetPersonTitleAction should not be called if the state is pouplated.', () => {
