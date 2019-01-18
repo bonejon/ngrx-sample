4368de7c10c9868c1521a22880ba66cbd9236d8e
Jon Bone <bonejon@gmail.com>
2019-01-18 12:13:29 +0000

    added no sandbox and angular flex-layout

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -1,23 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { Store } from '@ngrx/store';
 import { CommonState } from '../store/common/common.state';
 import { Observable } from 'rxjs';
 import { PersonTitle } from '../common/models/persontitle';
-import * as commonActions from '../store/common/common.actions';
 import { personTitlesSelector } from '../store/common/common.selectors';
 
 @Component({
     selector: 'app-profile',
     templateUrl: './profile.component.html'
 })
-export class ProfileComponent implements OnInit {
+export class ProfileComponent {
   public titles$: Observable<Array<PersonTitle>>;
 
   constructor(private store$: Store<CommonState>) {
     this.titles$ = store$.select(personTitlesSelector);
-  }
-
-  public ngOnInit(): void {
-    this.store$.dispatch(new commonActions.GetPersonTitlesAction());
   }
 }
